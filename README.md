f16374c80c460bde28a6518c865063c472e23a48
Brody Taylor <brodytaylor311@gmail.com>
2023-11-22 06:49:12 -0600

    Create setup and modify models and components to POST/GET chirps

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chirper-blade
+
+A CRUD server created using PHP's Laravel framework that focuses on the use of `chirps`.
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ChirpController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware('auth', 'verified');
+
+require __DIR__.'/auth.php';
